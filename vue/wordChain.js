d7d1c52b8238b191bd28da15a38fe13a0fe46e30
Park Saemi <45276842+saebak@users.noreply.github.com>
2019-09-28 23:54:22 +0900

    vue Exercise02
    vue Exercise01 word chain game
    last letter = first letter

--- vue/wordChain.js
+++ vue/wordChain.js
@@ -3,43 +3,43 @@
 <head>
 <meta charset="UTF-8">
 <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-<title>끝말잇기</title>
+<title>Insert title here</title>
 </head>
 <body>
 	<div id="root">
 		<div>{{word}}</div>
-		<form v-on:submit="onSubmitForm">
+		<form v-on:submit="onSubmitButton">
 			<input type="text" ref="answer" v-model="value">
 			<button type="submit">입력</button>
 		</form>
-		<div>{{result}}</div>
+		<div id="result"> {{result}} </div>
 	</div>
-	
-	<script>
-		const app = new Vue({
-			el: '#root',
-			data: {
-				word: '제로초',
-				result: '',
-				value: '',	
-			},
-			methods: {
-				onSubmitForm(){
-					e.preventDefault();
-					if(this.word[this.word.length-1] === this.value[0]){
-						this.result='딩동댕';
-						this.word = this.value;
-						this.value = '';
-						this.$refs.answer.focus();
-					}else{
-						this.result = '뗑';
-						this.value='';
-						this.$refs.answer.focus();
-					}
-				},
-			},
-		});
-	
-	</script>
 </body>
+
+<script>
+	const app = new Vue({
+		el : "#root",
+		data : {
+			word : '제로초',
+			value : '',
+			result : '',
+		},
+		methods : {
+			onSubmitButton(e){
+				e.preventDefault();
+				if(this.word[this.word.length-1] === this.value[0]){
+					this.result='딩동댕';
+					this.word = this.value;
+					this.value = '';
+					this.$refs.answer.focus();
+				}else{
+					this.result = '뗑';
+					this.value='';
+					this.$refs.answer.focus();
+				}
+			},
+		},
+	});
+
+</script>
 </html>
