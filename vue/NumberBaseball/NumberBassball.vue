95606e2f62b6fc5d61fd59a3a71743e272b87a40
Park Saemi <45276842+saebak@users.noreply.github.com>
2019-10-13 11:32:58 +0900

    webpack study
    vue.js webpack

---
+++ vue/NumberBaseball/NumberBassball.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<h1>{{result}}</h1>
+		<form v-on:submit="onSubmitForm">
+			<input ref="answer" maxlength="4" v-model="value"/>
+			<button>입력</button>
+		</form>
+	</div>
+</template>
+
+<script>
+	export_default{
+	data(){
+		return{
+			value: '',
+			result: '',
+		}
+	},
+	methods:{
+		onSubmitForm(e){
+			e.preventDefault();
+		}
+	}
+	};
+</script>
+
+<style>
+</style>
