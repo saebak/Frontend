692f3830cd7cf68318759d3886773e97e5b485d4
Park Saemi <45276842+saebak@users.noreply.github.com>
2019-10-13 11:31:40 +0900

    webpack study
    vue.js webpack

---
+++ vue/NumberBaseball/webpack_config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+	entry: {
+		app: path.join(__dirname,'main.js'),
+	},
+	module: {
+		rules:[{
+			
+		}],
+	},
+	plugins:[],
+	output:{
+		filename: '[name].js',
+		path: path.join(__dirname,'dist'),
+	},
+};
